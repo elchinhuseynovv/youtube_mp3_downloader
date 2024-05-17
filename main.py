66b5a00da3215b857d384487e41884348fa117ba
Elchin Huseynov <140554251+elchinhuseynovv@users.noreply.github.com>
2024-05-17 21:36:44 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+print("fly like whale")
+
+import pytube
+import youtube_downloader
+import file_converter
+
+choice = input("Choice: ")
+
+if choice == "1" or choice == "2":
+    quality = input("Choose a quality (low, medium, high, very high):")
+    if choice == "2":
+        link = input("Enter the link to the playlist: ")
+        print("Downloading playlist...")
+        youtube_downloader.download_playlist(link, quality)
+        print("Download finished!")
+    if choice == "1":
+        links = youtube_downloader.input_links()
+        for link in links:
+            youtube_downloader.download_video(link, quality)
+elif choice == "3":
+    links = youtube_downloader.input_links()
+    for link in links:
+        print("Downloading...")
+        filename = youtube_downloader.download_video(link, 'low')
+        print("Converting...")
+        file_converter.convert_to_mp3(filename)
+else:
+    print("Invalid input! Terminating...")
